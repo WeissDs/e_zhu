9b2f8ec57d33ee02b355560d59bb5ac8d8933ee2
daixiyu <82143372@qq.com>
2019-05-21 17:37:56 +0800

    在组件导入vuex后在computed中注册后使用vuex

--- e_cli/src/page/like.vue
+++ e_cli/src/page/like.vue
@@ -3,15 +3,19 @@
     <div class="have-no-like" v-if="$store.state.hotelCollect.length==0">
       您还没有收藏任何酒店 !
     </div>
-    <ShopItem v-for="(item,index) in $store.state.hotelCollect" :shopData="item" :key="index"></ShopItem>
+    <ShopItem v-for="(item,index) in hotelCollect" :shopData="item" :key="index"></ShopItem>
     <Foot/>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Foot from '../components/cmp-footer.vue'
 import ShopItem from '../components/cmp-home-shoplistItem'
 export default {
+  computed: {
+    ...mapState(['hotelCollect'])
+  },
   components: { Foot, ShopItem }
 }
 </script>
